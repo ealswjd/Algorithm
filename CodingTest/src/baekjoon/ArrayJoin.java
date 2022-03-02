ae316e876a7b8748dbb2344068381ff361240407
alswjd <79951187+ealswjd@users.noreply.github.com>
2022-03-03 00:01:54 +0900

    11728번 - 배열 합치기

--- CodingTest/src/baekjoon/ArrayJoin.java
+++ CodingTest/src/baekjoon/ArrayJoin.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main_02 {
+public class ArrayJoin {
 
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
