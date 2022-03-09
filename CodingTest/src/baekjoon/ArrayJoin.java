cb735ecad83e6526ae121fd97f13bf14e79febe5
alswjd <79951187+ealswjd@users.noreply.github.com>
2022-03-09 13:57:20 +0900

    11728번) 배열 합치기

--- CodingTest/src/baekjoon/ArrayJoin.java
+++ CodingTest/src/baekjoon/ArrayJoin.java
@@ -6,7 +6,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
-
+// https://www.acmicpc.net/problem/11728
+// 11728번) 배열 합치기
 public class ArrayJoin {
 
 	public static void main(String[] args) throws IOException {
