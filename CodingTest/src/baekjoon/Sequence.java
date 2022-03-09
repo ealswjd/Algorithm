79f5ea345f6fd5d1518aa647a87f61d61c423aab
alswjd <79951187+ealswjd@users.noreply.github.com>
2022-03-09 13:56:24 +0900

    2559번) 수열

--- CodingTest/src/baekjoon/Sequence.java
+++ CodingTest/src/baekjoon/Sequence.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-//수열 2559
+// https://www.acmicpc.net/problem/2559
+// 수열 2559
 public class Sequence {
 
     public static void main(String[] args) throws IOException {
