7bf6f943e36cc2730574a9eaf79b50de7a1cdd7b
alswjd <79951187+ealswjd@users.noreply.github.com>
2022-03-09 14:00:47 +0900

    11728번) 두 포인터 > 배열 합치기

--- CodingTest/src/baekjoon/ArrayJoin.java
+++ CodingTest/src/baekjoon/ArrayJoin.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 // https://www.acmicpc.net/problem/11728
-// 11728번) 배열 합치기
+// 11728번) 두 포인터 > 배열 합치기
 public class ArrayJoin {
 
 	public static void main(String[] args) throws IOException {
