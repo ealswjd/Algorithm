1c368846ae687f5e6e08a2dd0dfd1959708c13fc
alswjd <79951187+ealswjd@users.noreply.github.com>
2022-03-10 16:40:43 +0900

    Lv.1) 해시 > 완주하지 못한 선수

--- CodingTest/src/programmers/NotCompletionPlayer.java
+++ CodingTest/src/programmers/NotCompletionPlayer.java
@@ -3,8 +3,7 @@ package programmers;
 import java.util.HashMap;
 
 /* https://programmers.co.kr/learn/courses/30/lessons/42576
- * Lv.1) 해시 > 완주하지 못한 선수
- */
+ * Lv.1) 해시 > 완주하지 못한 선수                             */
 public class NotCompletionPlayer {
 
 	public String solution(String[] participant, String[] completion) {
